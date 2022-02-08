991e4f4d4202b468a66d1452c2cf8ac0a2be46c1
zjzjzjzj <zjzjzjzj1874@gmail.com>
2022-02-08 10:07:31 +0800

    chore🔧($main): something else
    Signed-off-by: zjzjzjzj <zjzjzjzj1874@gmail.com>

--- main.go
+++ main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Hello World!\n")
+	cards := deck{"Ace of Spades", newCard()}
+	cards = append(cards, "five of hearts")
+
+	for i, card := range cards {
+		fmt.Printf("%d,%s\n", i, card)
+	}
 }
+
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+type deck []string
