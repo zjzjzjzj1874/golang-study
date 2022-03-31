083332f82c8d70a2ead6d5917c81ef09e8e122a2
zjzjzjzj <zjzjzjzj1874@gmail.com>
2022-03-31 14:58:49 +0800

    feat:✨($closure): add closure with sum
    Signed-off-by: zjzjzjzj <zjzjzjzj1874@gmail.com>

--- src/base/base.go
+++ src/base/base.go
@@ -1,12 +1,10 @@
 package base
 
-import "fmt"
-
-// 数组相关
-func array() {
-	arr := [4]int{1}
-	arr1 := [...]int{1, 0, 0, 0}
-
-	fmt.Println(arr, arr1, len(arr), len(arr1))
-	fmt.Println(arr == arr1)
+// 闭包函数: 作用：缩小变量作用域，减少对全局变量的污染 => 不过以下的sum在编译阶段,会逃逸到堆内存上
+func closure() func(int) int {
+	var sum = 0
+	return func(a int) int {
+		sum += a
+		return sum
+	}
 }
--- src/base/base_test.go
+++ src/base/base_test.go
@@ -23,3 +23,25 @@ func Test_timeTicker(t *testing.T) {
 		time.Sleep(time.Minute)
 	})
 }
+
+// 闭包函数测试
+func Test_Closure(t *testing.T) {
+	// 计算1-10之和
+	t.Run("#closure闭包计算", func(t *testing.T) {
+		fn := closure()
+		sum := 0
+		for i := 1; i <= 10; i++ {
+			sum = fn(i) // sum也可以定义为局部变量,不过这样定义也好点,可以复用sum的地址,否则栈上内存创建又销毁
+		}
+		fmt.Printf("sum:%d\n", sum)
+	})
+	t.Run("#正常计算", func(t *testing.T) {
+		// 使用闭包计算1-10之和
+		sum := 0
+		for i := 1; i <= 10; i++ {
+			sum += i
+		}
+		fmt.Printf("sum:%d\n", sum)
+	})
+
+}
