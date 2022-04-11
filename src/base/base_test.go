3d58f029cb542348d94c9e226a077480df96a48f
zjzjzjzj <zjzjzjzj1874@gmail.com>
2022-04-11 17:30:54 +0800

    feat:✨(accounts): $puml
    ISSUES CLOSED: add some case
    
    Signed-off-by: zjzjzjzj <zjzjzjzj1874@gmail.com>

--- src/base/base_test.go
+++ src/base/base_test.go
@@ -51,7 +51,6 @@ func Test_Closure(t *testing.T) {
 		fmt.Printf("sum:%d\n", sum)
 	})
 	t.Run("#正常计算", func(t *testing.T) {
-		// 使用闭包计算1-10之和
 		sum := 0
 		for i := 1; i <= 10; i++ {
 			sum += i
